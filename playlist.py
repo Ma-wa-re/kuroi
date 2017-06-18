cc39ffa9be4a6112499654b8b4033496ad5c1d56
Luke Barr <lukebarr@outlook.com>
2017-06-18 11:00:57 -0500

    Enhanced summoning

--- playlist.py
+++ playlist.py
@@ -18,6 +18,7 @@ client = discord.Client()
 play_next_song = asyncio.Event()
 
 video = None
+voice = None
 
 def toggle_next():
     client.loop.call_soon_threadsafe(play_next_song.set)
@@ -29,12 +30,17 @@ async def on_ready():
 @client.event
 async def on_message(message):
     global video
-    if message.content.startswith('%summon') and message.author.id == "314215966903107586":
+    global voice
+    if message.content.startswith('%summon'):
         try:
             if message.author.voice_channel is None:
                 return False
             
-            voice = await client.join_voice_channel(message.author.voice_channel)
+            if voice is None:
+                voice = await client.join_voice_channel(message.author.voice_channel)
+            else:
+                await voice.move_to(message.author.voice_channel)
+                return False
 
             while True:
                 play_next_song.clear()
@@ -44,20 +50,20 @@ async def on_message(message):
                 await play_next_song.wait()
         except:
             pass
-    if 'youtube' in message.content and message.channel.id == "280778849615216640":
+    elif 'youtube' in message.content and message.channel.id == "280778849615216640":
         try:
             url = re.search("(?P<url>https?://[^\s]+)", message.content).group("url")
             param = re.search("(\?|\&)([^=]+)\=([^&]+)", url)[0]
-            if 'list' not in param and param not in playlist:
-                print(f"Appended video: {param[3:]}")
-                playlist.append(param[3:])
+            if 'list' not in param and param[3:] not in playlist:
+                param = param[3:]
+                print(f"Appended video: {param}")
+                playlist.append(param)
                 with open("videos.txt", "a+") as data:
-                    data.write(param[3:] + '\n') 
+                    data.write(param + '\n') 
         except Exception as e:
             print(e)
-    if message.content.startswith('%playing'):
+    elif message.content.startswith('%playing'):
         if video is not None and message.channel.id != "280778849615216640":
             await client.send_message(message.channel, f"https://www.youtube.com/watch?v={video}")
-            
 
 client.run("")
