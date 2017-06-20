cbecec921b042e21f4843347cd951548128423c2
Luke Barr <lukebarr@outlook.com>
2017-06-19 19:27:05 -0500

    something

--- Application.py
+++ Application.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 from discord.ext import commands
 
 if not discord.opus.is_loaded():
-    discord.opus.load_opus('opus')
+    discord.opus.load_opus("opus")
 
 class Radio:
     def __init__(self, bot, config, playlist=None):
@@ -19,7 +19,7 @@ class Radio:
         self.voice_channel = None
         self.skip_count = 0
         self.voters = []
-        self.vol = self.config['volume']
+        self.vol = self.config["volume"]
         self.play_next_song = asyncio.Event()
         if playlist is not None:
             self.playlist = playlist
@@ -31,16 +31,16 @@ class Radio:
         await self.bot.change_presence(game=discord.Game(name="%help"))
 
     async def on_message(self, message):
-        if 'youtube' in message.content and message.channel.id == self.config["music_channel"] and not message.author.bot:
+        if "youtube" in message.content and message.channel.id == self.config["music_channel"] and not message.author.bot:
             try:
                 url = re.search("(?P<url>https?://[^\s]+)", message.content).group("url")
                 param = urlparse(url).query
                 if param[0:2] != "v=":
                     param = "".join(("v=", param.split("v=")[1]))
-                if 'list' not in param and param[2:] not in self.playlist and param[2:] not in self.stack:
+                if "list" not in param and param[2:] not in self.playlist and param[2:] not in self.stack:
                     self.playlist.append(param[2:])
-                    with open(self.config['playlist_path'], "a") as data:
-                        data.write(param + '\n') 
+                    with open(self.config["playlist_path"], "a") as data:
+                        data.write(param + "\n") 
             except Exception as e:
                 print(e)
         self.bot.process_commands(message)
@@ -50,7 +50,7 @@ class Radio:
 
     @commands.command(pass_context=True, no_pm=True)
     async def volume(self, ctx, vol : int=None):
-        ''' Set the volume of the radio, any value between 0 and 100 '''
+        """ Set the volume of the radio, any value between 0 and 100 """
         if ctx.message.author.voice_channel is None or ctx.message.author.bot:
                 await self.bot.say("You are not in the voice channel!")
                 return False
@@ -69,9 +69,9 @@ class Radio:
 
     @commands.command(pass_context=True, no_pm=True)
     async def summon(self, ctx):
-        '''
+        """
         Summons the bot to your current voice channel
-        '''
+        """
         try:
             if ctx.message.author.voice_channel is None or ctx.message.author.bot:
                 return False
@@ -141,7 +141,7 @@ class Radio:
                 if user.bot:
                     bots_in_channel += 1
             current_user_amount = len(self.voice_channel.voice_members) - bots_in_channel
-            required_to_skip = int(current_user_amount+1 * self.config['skip_percentage'])
+            required_to_skip = int(current_user_amount+1 * self.config["skip_percentage"])
             
             await self.bot.say(f"{self.skip_count}/{required_to_skip} users have voted to skip!")
             if self.skip_count >= required_to_skip:
@@ -158,29 +158,31 @@ class Radio:
     async def playing(self, ctx):
         """Retrieves the currently playing song, if any."""
         if self.current_id is not None:
+            if self.current_id[:2] == "v=":
+                 self.current_id = self.current_id[2:]
+
 
             title = self.player.title
             url = self.player.url
-            mins, seconds = divmod(self.player.duration)
-            desc = (f'Duration: {mins}:{seconds}\n'
-                    f'Views: {self.player.views}\n'
-                    f'Uploader: {self.player.uploader}')
-            embed = discord.Embed(type='rich', title=title, url=url, description=desc)
-            embed.set_thumbnail(url=f'https://img.youtube.com/vi/{self.current_id}>/0.jpg')
+            mins, seconds = divmod(self.player.duration, 60)
+            desc = f"Duration: {mins}:{seconds}\nViews: {self.player.views}\nUploader: {self.player.uploader}"
+            embed = discord.Embed(type="rich", title=title, url=url, description=desc)
+            embed.set_thumbnail(url=f"https://img.youtube.com/vi/{self.current_id}/0.jpg")
+
             await self.bot.say(embed=embed)
 
 
 # Initalize
-with open('config.json', 'r') as f:
+with open("config.json", "r") as f:
     config = json.load(f)
 
 playlist = []
-with open(config['playlist_path'], "r") as data:
+with open(config["playlist_path"], "r") as data:
     for video in data:
-        playlist.append(video)
+        playlist.append(video.replace("\n", ""))
 
 shuffle(playlist)
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(config['prefix']), description=config['description'])
+bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), description=config["description"])
 bot.add_cog(Radio(bot, config, playlist))
-bot.run(config['token'])
+bot.run(config["token"])
